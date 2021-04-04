895eda1a25a99b352b3118aba406a31ea0fa26aa
Alejandro G. Fernández de Castro <a.gonzalez.fdez@gmail.com>
2021-04-04 21:01:31 +0200

    Add files via upload

---
+++ climatemodeling.R
@@ -0,0 +1,16 @@
+#0.PREPARACIÓN####
+packages<-c("tidyverse","sp","rgdal",
+                      "raster","DHARMa","mgcv")
+sapply(packages,require,character.only=TRUE)
+#1.Relativizar coordenadas####
+
+#ej.tenerife
+maxlat<-15
+maxlong<-28
+
+
+#readtmean
+#readprec
+#readtmax
+#evaptranstot
+pet<-batchPetHgsm(petfiles, 01, tmin, tmean, tmax, rad)
