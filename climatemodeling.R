80dc2c3424eabc7055a5c78e02b14f7724c6db21
Alejandro G. Fernández de Castro <a.gonzalez.fdez@gmail.com>
2021-04-04 22:07:27 +0200

    Add files via upload

--- climatemodeling.R
+++ climatemodeling.R
@@ -5,6 +5,8 @@ sapply(packages,require,character.only=TRUE)
 #1.Relativizar coordenadas####
 
 zi<-xi-min/max-min
+fields::Tps(x, Y, m = NULL, p = NULL, scale.type = "range", lon.lat = FALSE,
+    miles = TRUE, method = "GCV", GCV = TRUE, ...) #función thinplate
 
 
 
