91adc39cff231903878f9555dc7ea3c142f9fe25
Alejandro G. Fernández de Castro <a.gonzalez.fdez@gmail.com>
2021-04-07 11:24:06 +0200

    Add files via upload

--- climatemodeling.R
+++ climatemodeling.R
@@ -1,6 +1,6 @@
 #0.PREPARACIÓN####
 packages<-c("tidyverse","sp","rgdal",
-                      "raster","DHARMa","mgcv","fields","viridis","leaflet",
+            "raster","DHARMa","mgcv","fields","viridis","leaflet",
             "htmltools","htmlwidgets")
 sapply(packages,require,character.only=TRUE,quietly=TRUE)
 projectionUTM<-"+proj=utm +zone=28 +ellps=WGS84 +datum=WGS84 +units=m +no_defs "
@@ -10,24 +10,38 @@ projectiongeo<-"+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 
 GC<-readOGR("estacionesAEMET_GC.shp")
 GCgeo<-spTransform(GC,projectiongeo)
-limGC<-extent(GC)
-plot(GC)
-GCdf<-data.frame(GC) %>% mutate(LONG=LONG/10000,LAT=LAT/10000)
+coordinates(GCgeo)
+limGC<-extent(GCgeo)
+plot(GCgeo)
+GCdf<-data.frame(GC) %>% mutate(LONG=LONG/10000,LAT=LAT/10000) %>% 
+  select(-c(XUTM30,YUTM30,PROV_NOMBR,NOMHOJA,ALTI_ANE,PROV_ID,
+            NUM_CUENCA,HOJA_ID,GR_CUENCA_,TIPO_CORRI,AMBITO,
+            AMBITO_ID,CORRIENTE,CDR1,CDR2,optional,COMENTARIO,
+            coords.x1,coords.x2))
+GCdf$x<-coordinates(GCgeo)[,1]
+GCdf$y<-coordinates(GCgeo)[,2]
+tmean<-read_delim("TEMP.MEDIA MENSUAL.CSV",delim=";",skip=1)%>% 
+  subset(NOM_PROV=="LAS PALMAS") %>% na.omit() %>% mutate(x=LONGITUD/-100000,y=LATITUD/10000) %>% 
+  mutate(LONGITUD=NULL,LATITUD=NULL)  %>% subset(x<15*-1 & y>27.3) %>% 
+  group_by(Indicativo,NOMBRE)
 
-tmean<-read_delim("TEMP.MEDIA MENSUAL.CSV",delim=";",skip=1) %>% 
-  subset(NOM_PROV=="LAS PALMAS") %>% mutate(x=LONGITUD/-100000,y=LATITUD/10000) %>% 
-  mutate(LONGITUD=NULL,LATITUD=NULL) %>% rename(year=5) %>% subset(x<15*-1 & y>27.3)
+tmean<- tmean %>% summarise(across(enero:diciembre,mean),n=n(),across(x:y,max))
+
+GCdf<-GCdf %>% rename(Indicativo=IND_INM)
+GCdf[c("FID_","INDCTV","TIPO")]<-NULL
+GCdf[c("SGE","TIPO_CORR_","TOTANOS","TOTANOSC")]<-NULL
+GCdf[c("MUNI_ID")]<-NULL
+GCtmean<-full_join(GCdf,tmean,by="Indicativo",keep=TRUE,suffix=c("_GC","_AEMET"))
+GCtmean<-GCtmean %>% relocate(Indicativo_AEMET,.after=Indicativo_GC) %>% 
+relocate(NOMBRE,.after = LUGAR)
+  View(GCtmean)
+GCtmean %>% na.omit() %>% View()
+GCtmean %>% select(Indicativo_AEMET) %>% na.omit() %>% dim()
+GCtmean %>% select(Indicativo_GC) %>% na.omit() %>% dim()
 SpatialPointsDataFrame(coords=tmean[c("x","y")],data=tmean) %>% plot()
 points(GC)
 
-greenLeafIcon <- makeIcon(
-  iconUrl = "http://leafletjs.com/examples/custom-icons/leaf-green.png",
-  iconWidth = 38, iconHeight = 95,
-  iconAnchorX = 22, iconAnchorY = 94,
-  shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
-  shadowWidth = 50, shadowHeight = 64,
-  shadowAnchorX = 4, shadowAnchorY = 62
-)
+
 mapaweb<-leaflet() %>% 
   addTiles(group="Open Street Maps") %>%
   addProviderTiles(providers$Esri.WorldImagery,group="Ortoimágenes") %>% 
@@ -39,14 +53,14 @@ mapaweb<-leaflet() %>%
   addMarkers(data=GCdf,
              #group="Actuaciones LIFE CAÑADAS",
              #icon=iconoLIFE,
-             lng=GCdf$LONG,lat=GCdf$LAT, 
+             lng=GCdf$x,lat=GCdf$y, 
              popup = GCdf$LUGAR) %>% 
-  addMarkers(data=tmean,
+  #addMarkers(data=tmean,
              #group="Actuaciones LIFE CAÑADAS",
-             icon=greenLeafIcon,
-             lng=tmean$x,lat=tmean$y#,
-            # popup = tmean$NOMBRE
-             ) %>% 
+             # icon=greenLeafIcon,
+             # lng=tmean$x,lat=tmean$y#,
+             # # popup = tmean$NOMBRE
+  # ) %>% 
   
   
   
@@ -58,9 +72,13 @@ mapaweb<-leaflet() %>%
 saveWidget(mapaweb,file="mapaweb.html")
 #1.Relativizar coordenadas####
 
-zi<-xi-min/max-min
+predictores<-GCdf %>% select(x,y,ALT)
+precipitacion<-
 fields::Tps(x, Y, m = NULL, p = NULL, scale.type = "range", lon.lat = FALSE,
-    miles = TRUE, method = "GCV", GCV = TRUE, ...) #función thinplate
+            miles = TRUE, method = "GCV", GCV = TRUE, ...) #función thinplate
+
+zi<-xi-min/max-min
+
 
 
 
